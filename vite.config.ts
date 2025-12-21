86622333f41eea4222070d6a151ebb8ed893586d
Paul Balogh <paul.balogh@gmail.com>
2025-12-21 23:20:58 +0200

    Make vercel great again

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, type PluginOption } from "vite";
 
 export default defineConfig({
+	appType: "spa",
 	plugins: [tailwindcss() as unknown as PluginOption],
 });
